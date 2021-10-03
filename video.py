0b923e0cea6bb94606fc437d8ae1c99d3f169ade
miguelkoro <80132605+miguelkoro@users.noreply.github.com>
2021-10-03 23:37:13 +0200

    Update video.py

--- video.py
+++ video.py
@@ -3,7 +3,7 @@ import random
 from subprocess import PIPE, Popen, STDOUT
 
 def elegirEpisodio():
-    randomfile = random.choice(os.listdir("/home/pi/simpsonstv/videos/encoded")
+    randomfile = random.choice(os.listdir("/home/pi/simpsonstv/videos/encoded"))
     file = os.path.join('/home/pi/simpsonstv/videos/encoded', randomfile)
     playProcess = Popen(['omxplayer', '--aspect-mode', 'fill', file])
     playProcess.wait()
