a544020a27fc807b507e037aaf13ef33ee07a83d
miguelkoro <80132605+miguelkoro@users.noreply.github.com>
2021-10-03 23:32:38 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -7,6 +7,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.OUT)
 
+def turnOnScreen():
+    os.system('raspi-gpio set 19 op a5')
+    GPIO.output(18, GPIO.HIGH)
+
+turnOnScreen()
+    
 while (True):
     input = GPIO.input(26)
     if input == True:
